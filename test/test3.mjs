4e8ff24584b7becd2c610d0d4f14fb51d089d2cb
bhavani.velivala <bhavani.velivala@servicenow.com>
2022-09-12 18:41:16 +0530

    fixed test case fialures

--- test/test3.mjs
+++ test/test3.mjs
@@ -1,10 +1,6 @@
 // test3mjs
 import assert from 'assert';
 
-it('fail from an es module', () => {
-    assert.fail('boom');
-});
-
 it('does not match from an es module', () => {
-    assert.doesNotMatch('I will fail', /fail/);
+    assert.doesNotMatch('I will fail', /fails/);
 });
